ac185d8f5d3f30d61654ab688f58ed29ac9cdf06
Lauralee <legookoo@uwaterloo.ca>
2022-08-27 15:55:56 -0400

    add engine

--- api/app.js
+++ api/app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
     res.send('hello world!');
 });
 
+let port = process.env.port;
+if(port == null || port == "") {
+    port = 5000;
+}
+
+
 app.listen(port, () => {
     console.log('this is the start of our food shelter api on port 5000');
 });
